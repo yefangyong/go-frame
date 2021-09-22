fd66c2c7430763bb99c0925c4d67db67c9b735b7
yefangyong <1281074511@qq.com>
2021-09-22 19:06:58 +0800

    feat: init

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"go-frame/framework"
+	"net/http"
+)
+
+func main() {
+	server := &http.Server{
+		Handler: framework.NewCore(),
+		Addr:    "localhost:8080",
+	}
+	server.ListenAndServe()
+}
